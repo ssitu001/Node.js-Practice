74327f98240272052238ada9d8b378453477e7e3
Simon Situ <ssitu001@gmail.com>
2016-07-05 11:57:42 -0700

    seed dummy data in mondb

--- nodeTodo/models/toDoModels.js
+++ nodeTodo/models/toDoModels.js
@@ -1,5 +1,8 @@
+//what is mongoose? 
+//elegant mongodb object modeling for node.js
 var mongoose = require('mongoose');
 
+//
 var Schema = mongoose.Schema;
 
 var todoSchema = new Schema({
@@ -9,6 +12,7 @@ var todoSchema = new Schema({
   hasAttachment: Boolean
 });
 
+//compiling schema into a model, A model is a class with which we construct documents
 var Todos = mongoose.model('Todos', todoSchema);
 
 module.exports = Todos;
