0e47a6a292a9b39e762e2ac333c129b2f2b95d37
Simon Situ <ssitu001@gmail.com>
2016-07-05 12:54:47 -0700

    create api endpoints and test with postman

--- nodeTodo/app.js
+++ nodeTodo/app.js
@@ -3,6 +3,7 @@ var app = express();
 var mongoose = require('mongoose');
 var config = require('./config');
 var setupController = require('./controllers/setupControllers');
+var apiController = require('./controllers/apiController');
 
 //default port for express or 3000
 var port = process.env.PORT || 3000;
@@ -15,7 +16,9 @@ app.set('view engine', 'ejs');
 
 //database connection, mongodb is a single connection no need to disconnect
 mongoose.connect(config.getDbConnectionString());
+
 //imported a function from setupControllers
 setupController(app);
+apiController(app);
 
 app.listen(port);
