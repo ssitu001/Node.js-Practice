1b9b6f87e3f41f40270d49b6e0b67ee337069063
Simon Situ <ssitu001@gmail.com>
2016-07-04 22:05:56 -0700

    connect to mongodb via mongoose

--- nodeTodo/app.js
+++ nodeTodo/app.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var config = require('./config');
 
 var port = process.env.PORT || 3000;
 
 app.use('/assets', express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
+
+//database connection
+mongoose.connect(config.getDbConnectionString());
 
 app.listen(port);
