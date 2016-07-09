d3fc095fd401c5605f896ab7c429f982d3837837
Simon Situ <ssitu001@gmail.com>
2016-07-09 12:36:43 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app  = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  res.render('landing');
+});
+
+app.get('/campgrounds', function(req, res) {
+  var campgrounds = [
+    {name: "Yosemite", image: "https://upload.wikimedia.org/wikipedia/commons/0/06/Yosemite_USA.JPG"},
+    {name: "Sedona", image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR6HTjpOt9EISK7ISqzasZPdHJPsYM7bW9uP9v1YtppVzewFFoGQg"},
+    {name: "Mt Zion", image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRtvx9mnuG1-s5iTQi3W-AFtubttGFqu_WwE962AHL34B8svHMeCqNNp3nL"}
+  ];
+  res.render('campgrounds', {campgrounds: campgrounds});
+});
+
+app.get('/campgrounds/new', function(req, res) {
+  res.render('new');
+});
+
+app.post('/campgrounds', function(req, res) {
+  res.send("post!")
+});
+
+
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000!');
+})
