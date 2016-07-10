a9197a340b8a0320b1a59d34d6c7ce3b48a48d74
Simon Situ <ssitu001@gmail.com>
2016-07-10 14:00:15 -0700

    connect mongodb use mongoose

--- app.js
+++ app.js
@@ -1,40 +1,83 @@
 var express = require('express');
 var app  = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/yelpcamp');
   
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-var campgrounds = [
-  {name: "Yosemite", image: "https://upload.wikimedia.org/wikipedia/commons/0/06/Yosemite_USA.JPG"},
-  {name: "Sedona", image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR6HTjpOt9EISK7ISqzasZPdHJPsYM7bW9uP9v1YtppVzewFFoGQg"},
-  {name: "Mt Zion", image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRtvx9mnuG1-s5iTQi3W-AFtubttGFqu_WwE962AHL34B8svHMeCqNNp3nL"},
-  {name: "Yosemite", image: "https://upload.wikimedia.org/wikipedia/commons/0/06/Yosemite_USA.JPG"},
-  {name: "Sedona", image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR6HTjpOt9EISK7ISqzasZPdHJPsYM7bW9uP9v1YtppVzewFFoGQg"},
-  {name: "Mt Zion", image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRtvx9mnuG1-s5iTQi3W-AFtubttGFqu_WwE962AHL34B8svHMeCqNNp3nL"},
-  {name: "Yosemite", image: "https://upload.wikimedia.org/wikipedia/commons/0/06/Yosemite_USA.JPG"},
-  {name: "Sedona", image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR6HTjpOt9EISK7ISqzasZPdHJPsYM7bW9uP9v1YtppVzewFFoGQg"},
-  {name: "Mt Zion", image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRtvx9mnuG1-s5iTQi3W-AFtubttGFqu_WwE962AHL34B8svHMeCqNNp3nL"}
-];
+//Schema
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String
+});
+
+var Campground = mongoose.model('campground', campgroundSchema);
+
+Campground.create({
+  name: "Yosemite",
+  image: "https://upload.wikimedia.org/wikipedia/commons/0/06/Yosemite_USA.JPG",
+  description: "Best I ever had!"
+}, function(err, campground) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(campground);
+  }
+});
+
+// var campgrounds = [
+//   {name: "Yosemite", image: "https://upload.wikimedia.org/wikipedia/commons/0/06/Yosemite_USA.JPG"},
+//   {name: "Sedona", image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR6HTjpOt9EISK7ISqzasZPdHJPsYM7bW9uP9v1YtppVzewFFoGQg"},
+//   {name: "Mt Zion", image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRtvx9mnuG1-s5iTQi3W-AFtubttGFqu_WwE962AHL34B8svHMeCqNNp3nL"},
+//   {name: "Yosemite", image: "https://upload.wikimedia.org/wikipedia/commons/0/06/Yosemite_USA.JPG"},
+//   {name: "Sedona", image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR6HTjpOt9EISK7ISqzasZPdHJPsYM7bW9uP9v1YtppVzewFFoGQg"},
+//   {name: "Mt Zion", image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRtvx9mnuG1-s5iTQi3W-AFtubttGFqu_WwE962AHL34B8svHMeCqNNp3nL"},
+//   {name: "Yosemite", image: "https://upload.wikimedia.org/wikipedia/commons/0/06/Yosemite_USA.JPG"},
+//   {name: "Sedona", image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR6HTjpOt9EISK7ISqzasZPdHJPsYM7bW9uP9v1YtppVzewFFoGQg"},
+//   {name: "Mt Zion", image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRtvx9mnuG1-s5iTQi3W-AFtubttGFqu_WwE962AHL34B8svHMeCqNNp3nL"}
+// ];
 
 app.get('/', function(req, res) {
   res.render('landing');
 });
 
 app.get('/campgrounds', function(req, res) {
-  res.render('campgrounds', {campgrounds: campgrounds});
+  //Get all campgrounds from DB
+  Campground.find({}, function(err, allCampgrounds) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('campgrounds', {campgrounds: allCampgrounds});
+    }
+  });
 });
 
 app.get('/campgrounds/new', function(req, res) {
   res.render('new');
 });
 
+//SHOW more info about one campground
+app.get('/campgrounds/:id', function(req, res) {
+  res.send('show page');
+});
+
+
+
 app.post('/campgrounds', function(req, res) {
   var name = req.body.name;
   var image = req.body.image;
-  campgrounds.push({name: name, image: image});
-  res.redirect('/campgrounds');
+  var newCampground = {name: name, image: image};
+  Campground.create(newCampground, function(err, campground) {
+    if(err) {
+      console.log(err);
+    } else {
+      res.redirect('/campgrounds');
+    }
+  });
 });
 
 
